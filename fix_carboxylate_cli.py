d672159ed01c9ef2e6b21758fe3e98025bfadd5f
j-wags <jwagnerjpl@gmail.com>
2020-07-03 10:54:15 -0700

    change script to deal with mol2

--- fix_carboxylate_cli.py
+++ fix_carboxylate_cli.py
@@ -3,9 +3,9 @@ import sys
 from openforcefield.topology import Molecule
 
 if len(sys.argv) != 2:
-    print("python fix_carboxylate_cli.py file.sdf")
+    print("python fix_carboxylate_cli.py file.mol2")
     sys.exit()
 
 offmol = Molecule.from_file(sys.argv[1])
 fix_carboxylate_bond_orders(offmol)
-offmol.to_file(sys.argv[1], file_format='sdf')
+offmol.to_file(sys.argv[1], file_format='mol2')
