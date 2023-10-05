f00a38ef7a2429d422fabae1d5719ef66e1790ec
pbuslaev <pbuslaev@gmail.com>
2023-10-05 11:06:14 +0100

    further reduced n residues

--- test_parameterize_tripeptides.py
+++ test_parameterize_tripeptides.py
@@ -42,16 +42,22 @@ for folder in ['MainChain', 'CTerminal', 'NTerminal']:#, 'MainChain']:
     #prefix = os.path.join('tests', 'issue_2_c_term_charge', folder, 'PRO', 'PRO')
     #resnames = ['GLY', 'ALA', 'PHE']
     if (folder == 'MainChain'):
+      """
       resnames = [ 'ALA', 'ARG', 'ASH', 'ASN', 'ASP', 'GLH', 'GLN', 'GLU', 'GLY',  'HID', 'HIE', 'HIP',
                    'ILE', 'LEU', 'LYN', 'LYS', 'MET', 'PHE', 'PRO', 'SER', 'THR', 'TYR', 'VAL', 'TRP',]
                    #'CYX' ]
+      """
+      resnames = ['ARG', 'HID', 'HIE', 'HIP']
       #resnames = ['HIP', 'HIE', 'HID', 'GLY']
       #resnames = ['ALA', 'GLY', 'PRO', 'CYS']#, 'HIE', 'HID']
 
     else:
+      """
       resnames = [ 'ALA', 'ARG', 'ASN', 'ASP', 'GLN', 'GLU', 'GLY', 'HID', 'HIE', 'HIP',
                    'ILE', 'LEU', 'LYS', 'MET', 'PHE', 'PRO', 'SER', 'THR', 'TYR', 'VAL', 'TRP',
                    'CYX' ]
+      """
+      resnames = ['ARG', 'HID', 'HIE', 'HIP']
       #resnames = ['HIP', 'HIE', 'HID', 'GLY']
       #resnames = []
     for resa, resb in itertools.permutations(resnames, 2):
